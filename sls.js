5a566ec78ddad674f0f4046d768cb22b8f517f20
masx200 <masx200@qq.com>
2021-06-25 08:57:25 +0800

    格式化加安全设置

--- sls.js
+++ sls.js
@@ -13,20 +13,14 @@ const proxypoints = require("./proxypoints.js");
 const app = new Koa();
 const router = new KoaRouter();
 
-app.use(async (ctx, next) => {
-    ctx.res.setHeader("Strict-Transport-Security", "max-age=31536000");
-    return next();
-});
+app.use(httpssecure());
 app.use(range);
 app.use(logger());
 app.use(AccessControlAllowOrigin());
 app.use(cors({}));
 app.use(conditional());
 if (process.env.SERVERLESS) {
-    app.use(async (ctx, next) => {
-        ctx.req.headers["accept-encoding"] = "identity";
-        return next();
-    });
+    app.use(identity());
 }
 app.use(compress({}));
 
@@ -34,12 +28,34 @@ app.use(koastreametag({}));
 app.use(koaetag({}));
 proxypoints(app);
 
-router.get("/", async (ctx) => {
-    await sendFile(ctx, path.join(__dirname, "index.html"));
-});
+router.get("/", sendindex());
 
 app.use(router.allowedMethods());
 app.use(router.routes());
+
+function httpssecure() {
+    return async (ctx, next) => {
+        ctx.res.setHeader("Strict-Transport-Security", "max-age=31536000");
+        ctx.res.setHeader(
+            "Content-Security-Policy",
+            "upgrade-insecure-requests"
+        );
+        return next();
+    };
+}
+
+function identity() {
+    return async (ctx, next) => {
+        ctx.req.headers["accept-encoding"] = "identity";
+        return next();
+    };
+}
+
+function sendindex() {
+    return async (ctx) => {
+        await sendFile(ctx, path.join(__dirname, "index.html"));
+    };
+}
 
 function AccessControlAllowOrigin() {
     return async (ctx, next) => {
